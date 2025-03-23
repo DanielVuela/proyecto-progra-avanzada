0c08b2b995796ec4bf66505b3198bd70919a403a
EstebanQuesada <equesada00666@ufide.ac.cr>
2025-03-23 13:12:14 -0600

    Parte de la calculadora de calorías echo

---
+++ MacroBalance/Controllers/CalculadoraController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using MacroBalance.Database;  
+using MacroBalance.Service;  
+
+namespace MacroBalance.Controllers
+{
+    public class CalculadoraController : Controller
+    {
+        [HttpGet]
+        public ActionResult CalcularCalorias()
+        {
+           
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CalcularCalorias(Usuario model)
+        {
+            
+            using (var db = new MacroBalanceEntities2())
+            {
+                
+                var usuarioExistente = db.Usuario
+                    .FirstOrDefault(u => u.CorreoElectronico == model.CorreoElectronico);
+
+                if (usuarioExistente == null)
+                {
+                   
+                    db.Usuario.Add(model);
+                }
+                else
+                {
+                   
+                    usuarioExistente.Nombre = model.Nombre;
+                    usuarioExistente.Apellidos = model.Apellidos;
+                    usuarioExistente.FotoDePerfilUrl = model.FotoDePerfilUrl;
+                    usuarioExistente.FechaDeNacimiento = model.FechaDeNacimiento;
+                    usuarioExistente.Peso = model.Peso;
+                    usuarioExistente.Altura = model.Altura;
+                    usuarioExistente.NivelActividadFisica = model.NivelActividadFisica;
+                    usuarioExistente.Genero = model.Genero;
+                   
+                }
+
+                db.SaveChanges();
+
+             
+                int edad = 0;
+                if (model.FechaDeNacimiento.HasValue)
+                {
+                    edad = DateTime.Now.Year - model.FechaDeNacimiento.Value.Year;
+                    if (DateTime.Now.DayOfYear < model.FechaDeNacimiento.Value.DayOfYear)
+                        edad--;
+                }
+
+                double pesoDouble = (double)(model.Peso ?? 0m);
+                double alturaDouble = (double)(model.Altura ?? 0m) * 100;
+
+               
+                double tdee = CalculoCaloriasService.CalcularTDEE(
+                    pesoDouble,
+                    alturaDouble,
+                    edad,
+                    model.Genero,
+                    model.NivelActividadFisica
+                );
+
+                ViewBag.TDEE = tdee;
+                return View();
+            }
+        }
+    }
+}
---
+++ MacroBalance/Service/CalculoCaloriasService.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace MacroBalance.Service
+{
+    public static class CalculoCaloriasService
+    {
+        public static double CalcularTDEE(double pesoKg, double alturaCm, int edad, string genero, string nivelActividad)
+        {
+            double tmb;
+            if (!string.IsNullOrEmpty(genero) && genero.ToLower().Contains("masc"))
+            {
+                tmb = (10 * pesoKg) + (6.25 * alturaCm) - (5 * edad) + 5;
+            }
+            else
+            {
+                tmb = (10 * pesoKg) + (6.25 * alturaCm) - (5 * edad) - 161;
+            }
+
+            double factor = ObtenerFactorActividad(nivelActividad);
+            return tmb * factor;
+        }
+
+        private static double ObtenerFactorActividad(string nivelActividad)
+        {
+            if (string.IsNullOrWhiteSpace(nivelActividad))
+                return 1.2;
+
+            switch (nivelActividad.ToLower())
+            {
+                case "sedentario": return 1.2;
+                case "ligero": return 1.375;
+                case "moderado": return 1.55;
+                case "activo": return 1.725;
+                case "muy activo": return 1.9;
+                default: return 1.2;
+            }
+        }
+    }
+}
