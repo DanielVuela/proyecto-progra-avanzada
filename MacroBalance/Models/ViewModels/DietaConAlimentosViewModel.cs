f09e8962a8a5649820a6c231e41445591820e238
EstebanQuesada <equesada00666@ufide.ac.cr>
2025-04-21 00:28:33 -0600

    Revert "Sugerencias Y estado de animo"
    This reverts commit ba751b95a532cc02784b07293128850afcc5e2b6.

---
+++ MacroBalance/Models/ViewModels/DietaConAlimentosViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacroBalance.Models.ViewModels
+{
+    public class DietaConAlimentosViewModel
+    {
+        public string NombreDieta { get; set; }
+        public string DescripcionDieta { get; set; }
+
+        public List<AlimentoViewModel> Alimentos { get; set; }
+    }
+
+    public class AlimentoViewModel
+    {
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public decimal? Calorias { get; set; }
+        public decimal? Proteina { get; set; }
+        public decimal? Carbohidratos { get; set; }
+        public decimal? Grasas { get; set; }
+        public decimal? CantidadDiaria { get; set; }
+    }
+}
+
+
---
+++ MacroBalance/Models/ViewModels/DietaConObjetivoViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MacroBalance.Models.ViewModels
+{
+    public class DietaConObjetivoViewModel
+    {
+        public int IdDieta { get; set; }
+        public string NombreObjetivo { get; set; }
+        public string NombreDieta { get; set; }
+        public string DescripcionDieta { get; set; }
+    }
+}
