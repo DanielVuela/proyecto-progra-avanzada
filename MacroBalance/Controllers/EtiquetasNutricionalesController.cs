1500eed92321950496ce3c77dc77886b361d90fd
SusanHidalgoG <pamelahidalgogonzalez@gmail.com>
2025-04-21 14:07:32 -0600

    Etiquetas Nutricionales

---
+++ MacroBalance/Controllers/EtiquetasNutricionalesController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace MacroBalance.Controllers
+{
+    public class EtiquetasNutricionalesController : Controller 
+    {
+        public ActionResult Index()
+        {
+            return View("EtiquetasNutri");
+        }
+    }
+
+}
